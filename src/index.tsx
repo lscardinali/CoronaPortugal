5cf06a05dc25cdaeaad736ff95e9fddfa520a263
Lucas Salton Cardinali <lucas.cardinali@farfetch.com>
2020-03-27 03:15:01 +0000

    Removes Firestore support

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Plugins } from '@capacitor/core';
-import { updateUniqueHits } from './services/firestore';
 import ReactGA from 'react-ga';
-
-const { Storage } = Plugins;
 
 ReactGA.initialize('UA-67205656-4');
 
@@ -19,12 +15,3 @@ const meta = document.querySelector('meta[name="apple-mobile-web-app-status-bar-
 if (meta) {
     meta.setAttribute("content", window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "white");
 }
-
-const checkForFirstAccess = async () => {
-    const ret = await Storage.get({ key: 'firstAccess' });
-    if (ret.value === null) {
-        updateUniqueHits();
-    }
-}
-
-checkForFirstAccess();
